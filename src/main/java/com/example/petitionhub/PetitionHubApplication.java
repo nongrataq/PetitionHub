673b908f306bb863dfd38e61892f973725aabe4f
Bogdan <bogdan-747@mail.ru>
2025-04-10 23:10:11 +0300

    plug

---
+++ src/main/java/com/example/petitionhub/PetitionHubApplication.java
@@ -0,0 +1,13 @@
+package com.example.petitionhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetitionHubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetitionHubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/petitionhub/PetitionHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.petitionhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetitionHubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
