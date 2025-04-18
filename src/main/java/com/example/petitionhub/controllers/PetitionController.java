1178bb4d7bff748ba87f602a056b06e486c43f4f
Novice2311 <igordobrica60@gmail.com>
2025-04-19 00:29:06 +0300

    created a functions of adding petitions
    # Conflicts:
    #	src/main/java/com/example/petitionhub/configs/SecurityConfig.java
    
    # Conflicts:
    #	src/main/java/com/example/petitionhub/controllers/PetitionController.java
    #	src/main/java/com/example/petitionhub/repositories/PetitionRepository.java
    #	src/main/java/com/example/petitionhub/services/PetitionService.java
    #	src/main/resources/templates/main_page.ftlh

--- src/main/java/com/example/petitionhub/controllers/PetitionController.java
+++ src/main/java/com/example/petitionhub/controllers/PetitionController.java
@@ -1,6 +1,5 @@
 package com.example.petitionhub.controllers;
 
-import com.example.petitionhub.entities.PetitionEntity;
 import com.example.petitionhub.services.PetitionService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
@@ -10,31 +9,18 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/petitions")
 @RequiredArgsConstructor
-
 public class PetitionController {
-
     private final PetitionService petitionService;
 
     @PostMapping("/create")
     public String createPetition(@RequestParam String title, @RequestParam String description) {
         petitionService.createPetition(title, description);
-        return "redirect:/petitions/all-petitions";
+        return "redirect:/";
     }
 
     @GetMapping("/create-petition")
     public String getPetitions(Model model) {
-        model.addAttribute("petition", petitionService.getAllPetitions());
-        return "/petitions/petitions";
-    }
-
-    @GetMapping("/{id}")
-    public PetitionEntity getPetitionById(@PathVariable long id) {
-        return petitionService.getPetitionById(id);
-    }
-
-    @DeleteMapping("/{id}")
-    public void deletePetitionById(@PathVariable long id) {
-        petitionService.deletePetition(id);
+        return "/petitions/create-petitions";
     }
 
 }
